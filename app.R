24e057191ad420cb1ecd7e8159943d0d3f828c4f
vanichols <vnichols@iastate.edu>
2020-03-22 13:50:48 -0500

    new tab on shiny

--- app.R
+++ app.R
@@ -5,68 +5,124 @@ library(shinythemes) #--not working
 library(plotly)
 library(scales) #--to get $ on y axis, so easy!
 
+
+
+# data --------------------------------------------------------------------
+
 dat <- cyd_salprofs %>% 
     mutate(Total = total_salary_paid,
            Base = base_salary, 
-           Travel = travel_subsistence) %>% 
-    filter(prof_simp != "awarded prof")
+           Travel = travel_subsistence,
+           dept = str_to_title(dept)) %>% 
+    filter(prof_simp != "awarded prof") %>% 
+    mutate(prof_simp = str_to_title(prof_simp),
+           prof_simp = factor(prof_simp, levels = c("Asst Prof", "Assoc Prof", "Prof")))
+
+
+#-m pct
+dat_ns <- dat %>% 
+    select(fiscal_year, gender, id, prof_simp, dept, base_salary) %>% 
+    group_by(fiscal_year, dept, gender, prof_simp) %>% 
+    summarise(n = n()) %>% 
+    pivot_wider(names_from = gender, values_from = n) %>% 
+    mutate(M = replace_na(M, 0),
+           `F` = replace_na(`F`, 0)) %>% 
+    mutate(ntot = M + `F`,
+           mn_pct = M/ntot) %>%
+    select(-M, -`F`)
+
+
+#-m/f sal ratio
+dat_srat <- dat %>% 
+    select(fiscal_year, gender, prof_simp, dept, base_salary) %>%
+    group_by(fiscal_year, prof_simp, dept, gender) %>% 
+    summarise(base_salary = mean(base_salary, na.rm = T)) %>% 
+    pivot_wider(names_from = gender, values_from = base_salary) %>% 
+    mutate(mfsal_pct = (M/`F`)) %>% 
+    select(-`F`, -M) 
+
+
+dat2 <- 
+    dat_ns %>% 
+    left_join(dat_srat) %>% 
+    mutate(mfsal_pct = case_when(   #--if the dept is only one geneder, assign it a mfsal of 100 (perfectly equal)
+        mn_pct == 0 ~ 1,
+        mn_pct == 100 ~1,
+        is.na(mfsal_pct) ~ 1,
+        TRUE ~ mfsal_pct))
+
 
 dd_year <- dat %>% select(fiscal_year) %>% pull() %>% unique() %>% sort()
 dd_dept <- dat %>% select(dept) %>% pull() %>% unique() %>% sort()
 #dd_st <- c("Total", "Base", "Travel")
 
 
-ui <- fluidPage(title = "Salary Explorer",
-                theme = shinytheme("sandstone"), # <-- why isn't this working??
+
+
+
+ui <-
+    fluidPage(theme = shinytheme("sandstone"),
+              
+              navbarPage(
+                  "Iowa State Salary Data",
                 
-                plotlyOutput('plot'),
-                
-                hr(),
-                
-                fluidRow(column(2,
-                                h4("Something")
-                                # selectizeInput(inputId = 'myst', 
-                                #                label = 'Salary Type', 
-                                #                choices = dd_st, 
-                                #                selected = "Base",
-                                #                )
-                                ),
-                         column(
-                             5,
-                             selectizeInput('myy', 'Year', dd_year, "2018")
-                             ),
-                         column(
-                             5,
-                             selectizeInput('myd', 'Department', dd_dept, "agronomy")
-                             )
-                         )
-                )
-                
+                  tabPanel("Overview",
+                           plotlyOutput('plot2'),
+                           
+                           hr(),
+                           
+                           fluidRow(
+                               column(6,
+                                      selectizeInput('myy2', 'Year', dd_year, "2018")),
+                               column(6,
+                                      selectizeInput('myd2', 'Department to Highlight', dd_dept, "Agronomy"))
+                               )
+                           ),
+                  
+                  tabPanel(
+                      "Salaries",
+                      plotlyOutput('plot1'),
+                      
+                      # hr(),
+                      
+                      fluidRow(
+                          column(12,
+                                 h4("Hover over a point\nto see more info")),
+                          br(),
+                          column(6,
+                                 selectizeInput('myy1', 'Year', dd_year, "2018")),
+                          column(6,
+                                 selectizeInput('myd1', 'Department', dd_dept, "Agronomy"))
+                      )
+                  )
+                  )
+              )
 
 server <- function(input, output) {
 
-    dataset <- reactive({
+    dataset1 <- reactive({
 
         dat %>%
             select(fiscal_year, dept, prof_simp, gender, id, Total, Base, Travel) %>%
             pivot_longer(Total:Travel) %>%
-            filter(fiscal_year == input$myy) %>%
-            filter(dept == input$myd) %>%
+            filter(fiscal_year == input$myy1) %>%
+            filter(dept == input$myd1) %>%
             mutate(value = round(value, 0))
 
 
     })
 
-    output$plot <- renderPlotly({
+    output$plot1 <- renderPlotly({
 
         
         
-        p1 <- dataset() %>% 
+        p1 <- dataset1() %>% 
             ggplot(aes(gender, value, group = 1,
                        text = paste("Name:", id, "(", gender, ")",
                                     "<br>Salary: $", round(value/1000, digits = 0), "thou"))) +
             stat_summary(fun.y = mean, geom = "bar", aes(fill = gender)) + 
             geom_point() +
+            guides(fill = F) +
             facet_grid(name ~ prof_simp, scales = "free_y") +
             scale_y_continuous(labels = label_dollar())
         
@@ -74,6 +130,48 @@ server <- function(input, output) {
         
 
     })
+    
+    
+    
+    dataset2 <- reactive({
+        
+        dat2 %>%
+            filter(fiscal_year == input$myy2) %>%
+            mutate(mycolor = ifelse(dept == input$myd2, "Y", "N"))
+    })
+    
+    output$plot2 <- renderPlotly({
+        
+        
+        
+        p2 <- dataset2() %>% 
+            ggplot(aes(mfsal_pct, mn_pct,
+                       group = 1,
+                       text = paste("Dept:", dept,
+                                    "<br>Total Faculty:", ntot, ";", round(mn_pct*100, digits = 0), "% male",
+                                    "<br>Male Salary is", round(mfsal_pct*100, digits = 0), "% of Female"))) +
+            geom_hline(yintercept = 0.5) + 
+            geom_vline(xintercept = 1) +
+            geom_point(aes(size = ntot, color = mycolor, alpha = mycolor)) + 
+            geom_rug(color = "orange") +
+            guides(size = F, color = F) +
+            scale_color_manual(values = c("N" = "black", 
+                                          "Y" = "orange")) +
+            scale_alpha_manual(values = c("N" = 0.5,
+                                          "Y" = 1)) +
+            facet_grid(.~prof_simp) +
+            labs(x = "\nPercentage of Female Salary Earned by Males\n",
+                 y = "\nPercentage of Faculty Who Are Male\n") +
+            theme_pubclean() + 
+            theme(panel.grid = element_blank()) + 
+            scale_y_continuous(labels = label_percent()) +
+            scale_x_continuous(labels = label_percent()) 
+        
+        ggplotly(p2, tooltip = "text")
+        
+        
+    })
+    
 }
 
 shinyApp(ui, server)
--- static-plot-explore.R
+++ static-plot-explore.R
@@ -49,3 +49,64 @@ dataset %>%
 
 ggplotly(p1, tooltip = "text")
 
+
+# try on bi-plot ----------------------------------------------------------
+
+#--n values
+dat %>% 
+  select(fiscal_year, gender, id, prof_simp, dept, base_salary) %>% 
+  group_by(fiscal_year, dept) %>% 
+  mutate(n = n()) %>% 
+  arrange(dept, fiscal_year, prof_simp)
+
+#-m pct?
+neq <- dat %>% 
+  select(fiscal_year, gender, id, prof_simp, dept, base_salary) %>% 
+  group_by(fiscal_year, dept, gender, prof_simp) %>% 
+  summarise(n = n()) %>% 
+  pivot_wider(names_from = gender, values_from = n) %>% 
+    mutate(M = replace_na(M, 0),
+           `F` = replace_na(`F`, 0)) %>% 
+  mutate(ntot = M + `F`,
+         mn_pct = M/ntot) %>%
+  select(-M, -`F`)
+
+
+#-m/f sal ratio
+saleq <- dat %>% 
+  select(fiscal_year, gender, prof_simp, dept, base_salary) %>%
+  group_by(fiscal_year, prof_simp, dept, gender) %>% 
+  summarise(base_salary = mean(base_salary, na.rm = T)) %>% 
+  pivot_wider(names_from = gender, values_from = base_salary) %>% 
+  mutate(mfsal_pct = (M/`F`)) %>% 
+  select(-`F`, -M)
+
+library(ggpubr)
+dat2 <- 
+  neq %>% 
+  left_join(saleq) %>% 
+  mutate(mfsal_pct = case_when(   #--if the dept is only one geneder, assign it a mfsal of 100 (perfectly equal)
+    mn_pct == 0 ~ 1,
+    mn_pct == 100 ~1,
+    TRUE ~ mfsal_pct)) 
+  
+  ggplot(dat2, aes(mfsal_pct, mn_pct,
+             group = 1,
+             text = paste("Dept:", dept,
+                          "<br>Faculty:", round(mn_pct*100, digits = 0), "% male",
+                          "<br>Male Salary is", round(mfsal_pct*100, digits = 0), "% of Female"))) +
+    geom_hline(yintercept = 0.5) + 
+    geom_vline(xintercept = 1) +
+    geom_point(aes(size = ntot)) + 
+  geom_rug(color = "red") +
+    guides(size = F, color = F) +
+      facet_grid(prof_simp~.) +
+    theme_pubclean() + 
+    theme(panel.grid = element_blank()) + 
+  scale_y_continuous(labels = label_percent()) +
+    scale_x_continuous(labels = label_percent()) 
+    
+    theme_bw() #->p1 # <-- comment this out for exploring
+
+ggplotly(p1, tooltip = "text")
+
